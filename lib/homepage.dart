44f8fda583b4c070b02494f83759a20e281369ba
theeyefeye <dafa.hermawan.10@gmail.com>
2022-08-30 16:48:34 +0700

    first commit
    bismillah

---
+++ lib/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+          ),
+          Text("test"),
+        ],
+      ),
+    );
+  }
+}
